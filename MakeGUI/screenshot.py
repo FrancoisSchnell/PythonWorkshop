d22f2545cc9fa9da1fc75d8f71e1db6ba741d94d
Francois Schnell <schneo@gmail.com>
2022-10-16 21:41:16 +0200

    single screenshot

--- MakeGUI/screenshot.py
+++ MakeGUI/screenshot.py
@@ -5,14 +5,14 @@ import PySimpleGUI as sg
 screenshotNumber = 1
 
 # All the stuff inside your window.
-layout = [ [sg.Button('Screenshot!'), sg.Button('Cancel')] ]
+layout = [ [sg.Button('Screenshot manuel'), sg.Button('Cancel')] ]
 
 # Create the Window
 window = sg.Window('A screenshot tool!', layout, size=(300,50))
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == "Screenshot!":
+    if event == "Screenshot manuel":
         im = ImageGrab.grab()
         im.save("MakeGUI/screenshots/"+str(screenshotNumber)+".png")
         screenshotNumber += 1   
